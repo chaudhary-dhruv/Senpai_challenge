9c99063a359a10ce57a7430d959370d269d304fa
chaudhary-dhruv <dhruv.chaudhary.63513@gmail.com>
2025-07-30 01:16:40 +0530

    You are funny

--- app/src/main/java/com/example/senpaichallenge/ui/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/senpaichallenge/ui/auth/SignUpActivity.kt
@@ -1,4 +1,12 @@
 package com.example.senpaichallenge.ui.auth
 
-class SignUpActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.senpaichallenge.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
 }
