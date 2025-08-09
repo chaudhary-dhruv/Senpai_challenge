bc49afb9c3bf28783010c4c423e41bdc407a09e8
chaudhary-dhruv <dhruv.chaudhary.63513@gmail.com>
2025-08-09 23:09:16 +0530

    complete the splash and login

---
+++ app/src/main/java/com/example/senpaichallenge/ui/auth/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.senpaichallenge.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.senpaichallenge.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
